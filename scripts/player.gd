f419e689d317d3c3332eb27b5c482dee6e3e7f99
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-07-24 02:34:24 -0700

    player now has limited cells

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,11 +13,31 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # The static body to draw over
 @export var static_body: StaticBody2D
+@export var red_cells: int = 10
+@export var cyan_cells: int = 10
 
-var _do_flash: bool = false
+var flash_type: FlashType = FlashType.WHITE
+
+var _do_flash: FlashType = FlashType.NONE
 var _flash_angle: float
 var _flash_markers: Node
+var _beam_charge: float
 @onready var _camera: Camera2D = $Camera2D
+
+enum FlashType {
+	NONE,
+	WHITE,
+	RED,
+	CYAN,
+}
+
+
+func _process(_delta):
+	if flash_type == FlashType.RED:
+		if _beam_charge > 3.0 && _beam_charge <= red_cells:
+			_beam_charge += _delta
+			
+	pass
 
 
 func _physics_process(delta):
@@ -41,7 +61,7 @@ func _physics_process(delta):
 
 	# Do flash routine 
 	#  MUST BE DONE IN _physics_step()!!!
-	if _do_flash:
+	if !(_do_flash == FlashType.NONE || _do_flash == FlashType.RED):
 		_flash_light()
 
 
@@ -95,19 +115,70 @@ func _flash_light():
 	# flash_poly.color = Color(1,0,0,1)
 	flash_poly.polygon = PackedVector2Array(verts)
 	add_sibling(flash_poly)
-	_do_flash = false
+	_do_flash = FlashType.NONE
+
+func _has_enough_for_white() -> bool: return red_cells >= 1 && cyan_cells >= 1
+func _has_enough_for_red() -> bool: return red_cells >= 1
+func _has_enough_for_cyan() -> bool: return cyan_cells >= 5
 
 
+# Should cyan and red switch?
+#  Red should be the floaty stuff?
+#  Cyan should be the laser beam?
 func _input(event):
-	if event is InputEventMouse:
+	# Since there's probs only gonna be one place we'll need this, put it here
+	#  A check to see if we should do the flash or not
+	var _flash_check: Callable = func() -> bool:		
 		if event.is_action_pressed("player_flash"):
-			var cpos = Vector2.ZERO					# Top-left camera position
-			if _camera:
-				cpos = _camera.global_position - get_viewport().get_visible_rect().size / 2
-			var epos = cpos + event.position		# Event position relative to world space
-			var n = (epos - global_position).normalized()
-			if n.is_zero_approx():
-				_flash_angle= 0.0
-			else:
-				_flash_angle = rad_to_deg(atan2(n.y, n.x))
-			_do_flash = true
+			match flash_type:
+				FlashType.WHITE:
+					if _has_enough_for_white():
+						red_cells -= 1
+						cyan_cells -= 1
+						return FlashType.WHITE
+				FlashType.RED:
+					if _has_enough_for_red():
+						return FlashType.RED	# Do something else for red
+				FlashType.CYAN:
+					if _has_enough_for_cyan():
+						cyan_cells -= 5
+						return FlashType.CYAN
+				_:
+					return FlashType.NONE	
+		
+		return FlashType.NONE
+
+	if event is InputEventMouseButton:
+		# Do the red thing instead
+		if event.is_action_released("player_flash") && _do_flash == FlashType.RED:
+			if red_cells >= roundi(_beam_charge):
+				red_cells -= roundi(_beam_charge)
+
+			_do_flash = FlashType.NONE
+		else:
+			# Activate... the flash!
+			_do_flash = _flash_check.call()
+			if _do_flash:
+				_flash_angle = _calculate_flash_direction(event.position)
+	
+	if event is InputEventMouseMotion:
+		_flash_angle = _calculate_flash_direction(event.position)
+
+
+func _calculate_flash_direction(mouse_pos: Vector2):
+	var result: float = 0
+	var cpos = Vector2.ZERO					# Top-left camera position
+	if _camera:
+		cpos = _camera.global_position - get_viewport().get_visible_rect().size / 2
+	var epos = cpos + mouse_pos		# Event position relative to world space
+	var n = (epos - global_position).normalized()
+	if n.is_zero_approx():
+		result = 0.0
+	else:
+		result = rad_to_deg(atan2(n.y, n.x))
+		var q: float = round(result / 45.0)
+		q *= 45.0
+		result = q
+		
+	return result
+
