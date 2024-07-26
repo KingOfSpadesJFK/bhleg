cf33015b662ed0d6cc3b7e7d348815b7bf7ebc14
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-07-25 23:41:49 -0700

    scene manager addon

---
+++ addons/scene_manager/scenes.gd
@@ -0,0 +1,8 @@
+#
+# Please do not edit anything in this script
+#
+# Just use the editor to change everything you want
+#
+extends Node
+
+var scenes: Dictionary = {"_auto_refresh":false,"_auto_save":false,"_ignore_list":["res://addons","res://scenes"],"_ignores_visible":true,"_sections":["Scenes","Loadings"],"s1":{"sections":["Scenes"],"settings":{"All":{"subsection":"","visibility":true},"Scenes":{"subsection":"","visibility":true}},"value":"res://levels/s1.tscn"},"test_level":{"sections":["Scenes"],"settings":{"All":{"subsection":"","visibility":true},"Scenes":{"subsection":"","visibility":true}},"value":"res://levels/test_level.tscn"}}
